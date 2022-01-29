6258c458df2c6bfab71ef4d07c2f3318c1eacde2
Erick ZV <zv.erick@protonmail.com>
2022-01-28 18:28:01 -0600

    Refactor spread component

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -5,32 +5,44 @@ interface Props {
   websites: string[];
 }
 
+const NewsGroup: FC<{ webisteName: string }> = (props) => {
+  return (
+    <ul className="flex flex-col gap-2">
+      <p className="capitalize font-bold text-4xl text-white p-2">
+        {props.webisteName}
+      </p>
+      {props.children}
+    </ul>
+  );
+};
+
+const NewsItem: FC<newsInterface> = ({ href, title }) => {
+  return (
+    <li
+      className="text-xl p-2 max-w-max bg-teal-400 hover:bg-cyan-400
+      rounded hover:rounded-xl transition-all duration-500 ease-linear"
+    >
+      <a href={href} target="_blank" rel="noreferrer">
+        {title}
+      </a>
+    </li>
+  );
+};
+
 const NewsCard: FC<Props> = ({ news, websites }) => {
   return (
     <div className="flex flex-col gap-4">
       {/* Loops over group */}
       {news.length > 0 ? (
         news.map((group, groupIndex) => (
-          <ul key={groupIndex} className="flex flex-col gap-2">
-            <p className="capitalize font-bold text-4xl text-white p-2">
-              {websites[groupIndex]}:
-            </p>
-            {/* Loop over news title */}
-            {group.map((data, index) => (
-              <li
-                key={index}
-                className="text-xl p-2 max-w-max bg-teal-400 hover:bg-cyan-400
-                          rounded hover:rounded-xl transition-all duration-500 ease-linear"
-              >
-                <a href={data.href} target="_blank" rel="noreferrer">
-                  {data.title}
-                </a>
-              </li>
+          <NewsGroup key={groupIndex} webisteName={websites[groupIndex]}>
+            {group.map((data, ItemIndex) => (
+              <NewsItem key={ItemIndex} {...data} />
             ))}
-          </ul>
+          </NewsGroup>
         ))
       ) : (
-        <p className="capitalize text-center text-xl">no news available</p>
+        <p className="capitalize text-center text-xl">No News Available</p>
       )}
     </div>
   );
