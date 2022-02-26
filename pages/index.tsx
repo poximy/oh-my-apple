5acd6236476e53a4c2baa65602ff232dabf05d9c
Erick ZV <zv.erick@protonmail.com>
2022-02-25 21:01:12 -0600

    Fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import DaysSinceLaunch from "@components/DaysSinceLaunch";
 import NewsCard from "@components/NewsCard";
 import type { NextPage } from "next";
+import Head from "next/head";
 
 type Props = NewsData | null;
 
@@ -34,17 +35,22 @@ export async function getStaticProps() {
 
 const Home: NextPage<{ newsData: Props | null }> = ({ newsData }) => {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center m-4">
-      <h1 className="capitalize font-bold text-center text-6xl text-teal-400">
-        apple news
-      </h1>
-      <DaysSinceLaunch />
-      {newsData !== null ? (
-        <NewsCard {...newsData} />
-      ) : (
-        <p className="text-white text-6xl">Error No News Found :(</p>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>Apple Rumors</title>
+      </Head>
+      <div className="flex flex-col gap-8 justify-center items-center m-4">
+        <h1 className="capitalize font-bold text-center text-6xl text-teal-400">
+          apple rumors
+        </h1>
+        <DaysSinceLaunch />
+        {newsData !== null ? (
+          <NewsCard {...newsData} />
+        ) : (
+          <p className="text-white text-6xl">Error No News Found :(</p>
+        )}
+      </div>
+    </>
   );
 };
 
