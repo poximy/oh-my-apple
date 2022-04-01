898ab09c5b93fc01842ae1d90698387b988c3a38
poximy <76846708+poximy@users.noreply.github.com>
2022-04-01 12:18:11 -0600

    Add link icon

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -19,12 +19,22 @@ const NewsGroup: FC<{ websiteName: string }> = props => {
 const NewsItem: FC<newsInterface> = ({ href, title }) => {
   return (
     <li
-      className='rounded bg-teal-400
-      transition-all duration-300 ease-linear hover:scale-[1.025] hover:bg-cyan-400'
+      className='rounded bg-teal-400 transition-all duration-300 ease-linear
+      hover:scale-[1.025] hover:bg-cyan-400 relative group'
     >
       <a href={href} target='_blank' rel='noreferrer'>
         <p className='p-2 text-2xl md:text-xl'>{title}</p>
       </a>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        className='h-6 w-6 absolute top-0 right-0 group-hover:opacity-100 opacity-0
+        fill-gray-800'
+        viewBox='0 0 20 20'
+        fill='currentColor'
+      >
+        <path d='M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z' />
+        <path d='M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z' />
+      </svg>
     </li>
   );
 };
